7652eda170005db50b59d19f0cb5925f569c7ab8
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-20 13:41:00 -0300

    add dsl test runner

--- lib/macros/test_case.ex
+++ lib/macros/test_case.ex
@@ -2,13 +2,27 @@ defmodule MACROS.TestCase do
   defmacro __using__(_opts) do
     quote do
       import MACROS.TestCase
+      @tests []
+      @before_compile MACROS.TestCase
     end
   end
 
   defmacro test(description, do: block) do
     function_name = String.to_atom("test " <> description)
     quote do
+      @tests [unquote(function_name) | @tests]
       def unquote(function_name)(), do: unquote(block)
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      def run do
+        Enum.each @tests, fn name ->
+          IO.puts "Running #{name}"
+          apply(__MODULE__, name, [])
+        end
+      end
     end
   end
 end
--- test/macros/test_case_test.exs
+++ test/macros/test_case_test.exs
@@ -2,6 +2,12 @@ defmodule MACROS.TestCaseTest do
   use MACROS.TestCase
 
   test "hello" do
-    "hello" = "world"
+    "hello" = "hello"
+  end
+
+  test "arithmetic" do
+    3 = 1 + 2
   end
 end
+
+MACROS.TestCaseTest.run()
